a7ea9b948a6eb0af732349792ac6c3da7e0c3a41
avatarcounter <sofcatto@gmail.com>
2026-02-08 00:45:14 +0000

    Update coi-serviceworker.js

--- coi-serviceworker.js
+++ coi-serviceworker.js
@@ -1,17 +1,17 @@
 if (typeof window === "undefined") {
     self.addEventListener("install", () => self.skipWaiting());
-    self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
-    self.addEventListener("fetch", (event) => {
-        if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") return;
-        event.respondWith(
-            fetch(event.request).then((response) => {
-                if (response.status === 0) return response;
-                const newHeaders = new Headers(response.headers);
-                newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
-                newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
-                newHeaders.set("Cross-Origin-Resource-Policy", "cross-origin");
-                return new Response(response.body, { status: response.status, statusText: response.statusText, headers: newHeaders });
-            }).catch(e => console.error(e))
+    self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
+    self.addEventListener("fetch", (e) => {
+        if (e.request.cache === "only-if-cached" && e.request.mode !== "same-origin") return;
+        e.respondWith(
+            fetch(e.request).then((res) => {
+                if (res.status === 0) return res;
+                const h = new Headers(res.headers);
+                h.set("Cross-Origin-Embedder-Policy", "require-corp");
+                h.set("Cross-Origin-Opener-Policy", "same-origin");
+                h.set("Cross-Origin-Resource-Policy", "cross-origin");
+                return new Response(res.body, { status: res.status, statusText: res.statusText, headers: h });
+            }).catch(err => console.error(err))
         );
     });
 }
