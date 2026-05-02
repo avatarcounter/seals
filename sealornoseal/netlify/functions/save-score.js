e38aff398699155877613fb23e5f5444b8d45446
avatarcounter <sofcatto@gmail.com>
2026-05-02 17:38:06 +0100

    Update save-score.js

--- sealornoseal/netlify/functions/save-score.js
+++ sealornoseal/netlify/functions/save-score.js
@@ -1,15 +1,11 @@
 exports.handler = async (event) => {
     const PRIVATE_KEY = process.env.DREAMLO_PRIVATE_KEY;
     const { name, score } = JSON.parse(event.body);
-    const url = `http://dreamlo.com{PRIVATE_KEY}/add/${name}/${score}`;
+    // Change http to https
+    const url = `https://dreamlo.com{PRIVATE_KEY}/add/${name}/${score}`;
 
     try {
         const response = await fetch(url);
-        
-        if (!response.ok) {
-            return { statusCode: response.status, body: "Error saving score" };
-        }
-
         return {
             statusCode: 200,
             body: JSON.stringify({ message: "Score saved!" })
