225b1012372118262474011f58eb1de8c8a19078
avatarcounter <sofcatto@gmail.com>
2026-05-02 19:39:14 +0100

    Update get-scores.js

--- netlify/functions/get-scores.js
+++ netlify/functions/get-scores.js
@@ -1,18 +1,21 @@
+// netlify/functions/get-scores.js
 exports.handler = async () => {
-    // UPDATED with your new Public Key
     const PUBLIC_KEY = "69f639198f40bb1068bb7e9f";
     const url = `http://dreamlo.com{PUBLIC_KEY}/json`;
 
     try {
+        // NATIVE FETCH (No require needed in Node 18+)
         const response = await fetch(url);
+        
+        if (!response.ok) {
+            return { statusCode: response.status, body: "Dreamlo Error" };
+        }
+
         const data = await response.json();
 
         return {
             statusCode: 200,
-            headers: { 
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*" 
-            },
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify(data)
         };
     } catch (error) {
