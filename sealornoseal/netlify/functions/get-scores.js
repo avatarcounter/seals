a0f231915c49cbd8f28e1d7cc95ddaa6e05eca64
avatarcounter <sofcatto@gmail.com>
2026-05-02 16:58:51 +0100

    Update get-scores.js

--- sealornoseal/netlify/functions/get-scores.js
+++ sealornoseal/netlify/functions/get-scores.js
@@ -1,22 +1,25 @@
 const fetch = require('node-fetch');
 
 exports.handler = async () => {
-    // Your Dreamlo Public Key
     const PUBLIC_KEY = "69f60e2d8f40bb1068b944a4";
     
-    // We call HTTP here to avoid the SSL error
+    // Fixed: Added "/" and the missing "$" for the template literal
     const url = `http://dreamlo.com{PUBLIC_KEY}/json`;
     
     try {
         const response = await fetch(url);
+        
+        if (!response.ok) {
+            throw new Error(`Dreamlo responded with status: ${response.status}`);
+        }
+
         const data = await response.json();
 
         return {
             statusCode: 200,
             headers: {
                 "Content-Type": "application/json",
-                // This allows your HTML page to talk to this function
-                "Access-Control-Allow-Origin": "*"
+                "Access-Control-Allow-Origin": "*" 
             },
             body: JSON.stringify(data)
         };
@@ -24,7 +27,8 @@ exports.handler = async () => {
         console.error("Error fetching scores:", err);
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: "Failed to fetch scores from Dreamlo" })
+            headers: { "Access-Control-Allow-Origin": "*" },
+            body: JSON.stringify({ error: err.message })
         };
     }
 };
