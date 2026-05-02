13fe311a03e4e8a15d2914890bedf533802221e5
avatarcounter <sofcatto@gmail.com>
2026-05-02 20:27:42 +0100

    Delete api directory

--- api/save-score.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
-  const { name, score } = req.body;
-  const PRIVATE_KEY = "waBsY9hwf0urcEpwwn8rLw1Z2xYyPvZEqGdwAay68VqQ";
-  await fetch(`http://dreamlo.com{PRIVATE_KEY}/add/${encodeURIComponent(name)}/${score}`);
-  res.status(200).json({ success: true });
-}
