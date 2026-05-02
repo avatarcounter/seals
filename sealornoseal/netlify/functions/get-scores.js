25624191da0f76bf1a9c1bc465f3378d09925990
avatarcounter <sofcatto@gmail.com>
2026-05-02 17:37:31 +0100

    Update get-scores.js

--- sealornoseal/netlify/functions/get-scores.js
+++ sealornoseal/netlify/functions/get-scores.js
@@ -1,6 +1,7 @@
 exports.handler = async () => {
     const PUBLIC_KEY = "69f60e2d8f40bb1068b944a4";
-    const url = `http://dreamlo.com{PUBLIC_KEY}/json`;
+    // Change http to https and ensure /lb/ is included
+    const url = `https://dreamlo.com{PUBLIC_KEY}/json`;
 
     try {
         const response = await fetch(url);
