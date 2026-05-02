3839ecaf3686733f009055ec8018647265126377
avatarcounter <sofcatto@gmail.com>
2026-05-02 20:42:49 +0100

    Delete sealornoseal/api directory

--- sealornoseal/api/get-scores.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  const PUBLIC_KEY = "69f60e2d8f40bb1068b944a4";
-  const response = await fetch(`http://dreamlo.com{PUBLIC_KEY}/json`);
-  const data = await response.json();
-  res.status(200).json(data);
-}
